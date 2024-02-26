f95993cb4bf0201ab6606ce3defbb1cf55093def
Folorunso Timson <timsonfoli@gmail.com>
2024-02-26 12:33:50 +0100

    My first primitive data type teaching in python at Aptech

---
+++ new-class.py
@@ -0,0 +1,21 @@
+# name = "Sis Tolu"
+# print(name)
+
+# title="sis "
+# name = "tolu"
+# both = title + name
+# print(both.capitalize())
+
+
+# num1= 2
+
+# num3=2
+
+# num1=num3
+
+
+input()
+print("Sis tolu")
+
+
+print('sis tolu')
